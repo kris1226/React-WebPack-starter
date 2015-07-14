fbbb37b5f91f3ec1d0ad9c9d792b919dfa059273
Kristopher <kris1226@gmail.com>
2015-07-13 23:45:08 -0400

    inital commit

---
+++ components/Login.js
@@ -0,0 +1,14 @@
+'use strict';
+import React from 'react';
+
+let Login = React.createClass({
+  render() {
+    return(
+      <div>
+        Welcome to login component!
+      </div>
+    );
+  }
+});
+
+export default Login;
---
+++ js/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+// First import all the packages for react and react router
+import React from 'react';
+import Router from 'react-router';
+import { DefaultRoue, Link, Route, RouteHandler } from 'react-router';
+
+import LoginHandler from './components/Login.js';
+
+let App = React.createClass({
+  render() {
+    return (
+      <div className="nav">
+        <Link to="app">Home</Link>
+        <Link to="login">Login</Link>
+
+        {/* this is the important part */}
+        <RouteHandler/>
+      </div>
+    );
+  }
+});
+
+let routes = (
+  <Route name="app" path='/' handler={App}>
+    <Route name="login" path='/login' handler={LoginHandler}/>
+  </Route>
+);
+
+Router.run(routes, function(Handler) {
+  React.render(<Handler/>, document.body);
+});
